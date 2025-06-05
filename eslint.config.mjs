9fe55aed09fa7fd30aaf7c4a4cbd037dce76e637
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2025-06-05 12:19:31 -0400

    Updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,47 @@
 import antfu from "@antfu/eslint-config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default antfu(
-  {
-    type: "app",
-    typescript: true,
-    formatters: true,
-    stylistic: {
-      indent: 2,
-      semi: true,
-      quotes: "double",
-    },
-    ignores: [".pnpm-store/*", "node_modules"],
-  },
-  {
-    rules: {
-      "ts/no-redeclare": "off",
-      "ts/consistent-type-definitions": ["error", "type"],
-      "no-console": ["warn"],
-      "antfu/no-top-level-await": ["off"],
-      "node/prefer-global/process": ["off"],
-      "node/no-process-env": ["error"],
-      "perfectionist/sort-imports": [
-        "error",
-        {
-          tsconfigRootDir: ".",
-        },
-      ],
-      "unicorn/filename-case": [
-        "error",
-        {
-          case: "kebabCase",
-          ignore: ["README.md"],
-        },
-      ],
-    },
-  },
+	{
+		type: "app",
+		typescript: true,
+		formatters: true,
+		stylistic: {
+			// indentStyle: "tab", // Ensure tabs are used instead of spaces
+			indentWidth: 2, // Optional: only affects display width of tabs
+			semi: true,
+			quotes: "double",
+		},
+		ignores: [".pnpm-store/*", "node_modules", "**/*.json"],
+	},
+	{
+		rules: {
+			"ts/no-redeclare": "off",
+			"ts/consistent-type-definitions": ["error", "type"],
+			"no-console": ["warn", { allow: ["info"] }],
+			"antfu/no-top-level-await": ["off"],
+			"node/prefer-global/process": ["off"],
+			"perfectionist/sort-imports": [
+				"error",
+				{
+					tsconfigRootDir: ".",
+				},
+			],
+			"unicorn/filename-case": [
+				"error",
+				{
+					case: "kebabCase",
+					ignore: ["README.md"],
+				},
+			],
+			"prettier/prettier": [
+				"error",
+				{
+					useTabs: true,
+					endOfLine: "auto",
+				},
+			],
+		},
+	},
+	eslintPluginPrettierRecommended,
 );
