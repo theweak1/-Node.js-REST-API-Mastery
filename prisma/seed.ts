9eb8d495a9f65a6cd152f297ed9da6e9cef9359c
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2025-06-11 15:23:52 -0400

    Updated userId creation to be with uuid function

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,11 +7,11 @@ function capitalize(word: string) {
 	return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-const userIds = [
-	"216c1653-7b13-49bd-9499-53007ead0126",
-	"0cdbed84-0b12-4b89-91ae-5572e8e1258e",
-	"4dd4510b-2b01-438d-be7a-0064460230a1",
-];
+const userIds: string[] = [];
+
+for (let i = 0; i < 3; i++) {
+	userIds.push(faker.string.uuid());
+}
 
 async function main() {
 	for (const userId of userIds) {
